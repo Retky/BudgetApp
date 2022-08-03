11ca366340747dc75194e249ebe507427eb9b623
Retky <jose.arredondo.a@outlook.com>
2022-08-02 20:49:13 -0500

    Add 'New Category' view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,26 @@ class CategoriesController < ApplicationController
 
     render :index
   end
+
+  def new
+    @title = 'New Category'
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
